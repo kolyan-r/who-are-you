750e35e8273d8e2e89a7b6d5ca17b16088dece75
Niko <kolyan-r@ya.ru>
2018-04-18 21:38:03 +0300

    init

--- index.php
+++ index.php
@@ -1,7 +1,42 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kozy
- * Date: 18.04.18
- * Time: 21:13
- */
+
+if (!empty($_GET['action'])) {
+    $action = $_GET['action'];
+} else {
+    $action = 'log';
+}
+
+$logPath = __DIR__ . '/logs/access.log';
+
+switch ($action) {
+    case 'log':
+
+        $clientInfo = [
+            'server' => $_SERVER,
+            'session' => $_SESSION,
+            'request' => [
+                'GET' => $_GET,
+                'POST' => $_POST
+            ],
+            'cookie' => $_COOKIE,
+            'files' => $_FILES
+        ];
+
+        $logMessage = [];
+        $logMessage[] = date('Y-m-d H:i:s');
+        $logMessage[] = '***********************';
+        $logMessage[] = json_encode($clientInfo);
+        $logMessage[] = '***********************';
+
+        file_put_contents($logPath, implode(PHP_EOL, $logMessage));
+
+        include_once __DIR__ . '/stub.php';
+
+        break;
+    case 'read':
+        echo file_get_contents($logPath);
+        break;
+    case 'flush':
+        file_put_contents($logPath, '');
+        echo 'flushed';
+}
